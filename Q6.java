c3857770b1167ab4c30979889db429828aca39f7
MartinRavenel <matt-76R@hotmail.com>
2025-10-08 16:29:15 +0200

    Create Q6.java

---
+++ Q6.java
@@ -0,0 +1,12 @@
+public class Q6{
+  public static void main(String[] args){
+    String txt = "";
+    for( int i=args[0]; i=<0 ; i -- ){
+      for( int j=args[0]; j=>0 ; j -- ){
+        txt += "x " ;
+      }
+      txt += "\n" ;
+    }
+    System.out.println(txt);
+  }
+}
