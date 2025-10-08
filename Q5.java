f02a1064868073754a1cc7269de4a9843c615e3a
MartinRavenel <matt-76R@hotmail.com>
2025-10-08 16:28:28 +0200

    Update Q5.java

--- Q5.java
+++ Q5.java
@@ -2,7 +2,7 @@ public class Q5{
   public static void main(String[] args){
     String txt = "";
     for( int i=args[0]; i=<0 ; i -- ){
-      for( int j=0; j=>args[0] ; j ++ ){
+      for( int j=args[0]; j=>0 ; j -- ){
         txt += "x " ;
       }
       txt += "\n" ;
